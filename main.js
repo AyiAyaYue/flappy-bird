653d9e4f87c7b898eba9f91f1aa6a0c4741d154a
Li,Yunyue Y <cathylee599@gmail.com>
2021-09-18 15:42:03 +0200

    Obstacles of Random Size

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function component(width, height, color, x, y) {
 }
 
 function updateGameArea() { //the component is now drawn and clear 50 times per sec
-    let x, y;
+    let x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {   //loop through every obstacle to see if there is a crash
         if (myGamePiece.crashWith(myObstacles[i])) {
             myGameArea.stop();
@@ -70,8 +70,14 @@ function updateGameArea() { //the component is now drawn and clear 50 times per 
     myGameArea.frameNo += 1;
     if (myGameArea.frameNo == 1 || everyinterval(150)) {
         x = myGameArea.canvas.width;
-        y = myGameArea.canvas.height - 200;
-        myObstacles.push(new component(10, 200, "green", x, y));
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        myObstacles.push(new component(10, height, "green", x, 0));
+        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].x += -1;
