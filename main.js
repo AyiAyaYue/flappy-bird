64293f60ae33ff29de6ed44d089957e935453db5
Li,Yunyue Y <cathylee599@gmail.com>
2021-09-21 20:08:13 +0200

    Physics

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-let myGamePiece;
-let myObstacles = [];
-let myScore;
+let player;
+let obstacles = [];
+let score;
 
 function startGame() {
-    myGameArea.start(); //  make a gaming area
-    myGamePiece  = new component(30, 30, "img/giphy.gif", 10, 120, 'image'); //make a component
-    //myObstacle = new component(10, 200, "green", 300, 120); //make a obstacle
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    game.start();
+    player  = new component(30, 30, "img/giphy.gif", 10, 120, 'image');
+    score = new component("30px", "Consolas", "black", 280, 40, "text");
 }
 
-let myGameArea = {
+let game = {
     canvas: document.createElement('canvas'),
     start: function() {
         this.canvas.width = 480;
@@ -17,13 +16,16 @@ let myGameArea = {
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0; //counting frames
-        this.interval = setInterval(updateGameArea, 20);  //update the display every 20th millisecond (50 times persecond)
+
+
+        // this.interval = setInterval(updateGameArea, 20);  //update the display every 20th millisecond (50 times persecond)
+        requestAnimationFrame(updateGameArea);
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }, // clears the entire canvas
     stop: function() {
-        clearInterval(this.interval);
+        // clearInterval(this.interval);
     }
 }
 
@@ -37,12 +39,14 @@ function component(width, height, color, x, y, type) {
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
+    this.accelerationY  = 0;
+    this.clampY = 0.9;
     this.x = x;
     this.y = y;
-    this.gravity = 0.05;
+    this.gravity = 180; // px/s
     this.gravitySpeed = 0;
     this.update = function() {
-        ctx = myGameArea.context;
+        ctx = game.context;
         
         if (type == 'text') {
             ctx.font = this.width + " " + this.height;
@@ -62,14 +66,19 @@ function component(width, height, color, x, y, type) {
                 this.height);
         }
     }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
+    this.newPos = function(dt) {
+        // Update velocity
+        this.speedY += -this.accelerationY;
+        this.speedY *= this.clampY; // clamping
+
+        // Update position
         this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
+        this.y += (this.speedY + this.gravity) * dt;
+
         this.hitBottom();
     }
     this.hitBottom = function() {
-        let rockBottom = myGameArea.canvas.height - this.height;
+        let rockBottom = game.canvas.height - this.height;
         if (this.y >  rockBottom) {
             this.y = rockBottom;
         }
@@ -91,50 +100,58 @@ function component(width, height, color, x, y, type) {
     }
 }
 
+let currentTime = performance.now();
+
 function updateGameArea() { //the component is now drawn and clear 50 times per sec
+    requestAnimationFrame(updateGameArea);
+
+    const newTime = performance.now();
+    const dt = (newTime - currentTime) / 1000; // deltaTime in seconds
+    currentTime = newTime;
+
     let x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {   //loop through every obstacle to see if there is a crash
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            myGameArea.stop();
+    for (i = 0; i < obstacles.length; i += 1) { //loop through every obstacle to see if there is a crash
+        if (player.crashWith(obstacles[i])) {
+            game.stop();
             return;
         } 
     }
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
+    game.clear();
+    game.frameNo += 1;
+    if (game.frameNo == 1 || everyinterval(150)) {
+        x = game.canvas.width;
         minHeight = 20;
         maxHeight = 200;
         height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
         minGap = 50;
         maxGap = 200;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+        obstacles.push(new component(10, height, "green", x, 0));
+        obstacles.push(new component(10, x - height - gap, "green", x, height + gap));
     }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
+    for (i = 0; i < obstacles.length; i += 1) {
+        obstacles[i].x += -1;
+        obstacles[i].update();
     }
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();    
-    myGamePiece.update();
+    score.text="SCORE: " + game.frameNo;
+    score.update();
+    player.newPos(dt);    
+    player.update();
 
-    /* if (myGamePiece.crashWith(myObstacle)) {
-        myGameArea.stop();                      //if it hits the obstacle, game stop
+    /* if (player.crashWith(obstacle)) {
+        game.stop();                      //if it hits the obstacle, game stop
     }  else {
-        myGameArea.clear();
-        myObstacle.x += -1;
-        myObstacle.update();
-        myGamePiece.newPos();
-        myGamePiece.update();                   // else do something
+        game.clear();
+        obstacle.x += -1;
+        obstacle.update();
+        player.newPos();
+        player.update();                   // else do something
     } */
 }
 
 //a method for execute something at a given frame rate.
 function everyinterval(n){
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    if ((game.frameNo / n) % 1 == 0) {return true;}
     return false;
 }
 
@@ -142,22 +159,18 @@ function everyinterval(n){
 window.addEventListener('keydown', fly);
 window.addEventListener('keyup', flyDown);
 
-function fly(e) {
-    if (e.defaultPrevented) {
-        return;
-    }
+let isSpaceDown = false;
+let impulse = -600;
 
-    if (e.code === "Space"){
-        myGamePiece.gravity = -0.2;
+function fly(e) {
+    if (e.code === "Space" && !isSpaceDown) {
+        isSpaceDown = true;
+        player.speedY = impulse;
     } 
 }
 
 function flyDown(e) {
-    if (e.defaultPrevented) {
-        return;
-    }
-
-    if (e.code === "Space"){
-        myGamePiece.gravity = 0.05;
+    if (e.code === "Space" && isSpaceDown) {
+        isSpaceDown = false;
     } 
 }
